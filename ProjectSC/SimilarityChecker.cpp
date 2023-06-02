72fd1176863978cdfd34c8c707d68d7ebf660be0
Seokhwan <sukkakim@gmail.com>
2023-06-02 14:04:52 +0900

    add getting length score and unit tests

--- ProjectSC/SimilarityChecker.cpp
+++ ProjectSC/SimilarityChecker.cpp
@@ -1,5 +1,47 @@
+#include <string>
+using namespace std;
 class SimilarityChecker
 {
 public:
+	SimilarityChecker() = default;
+	SimilarityChecker(string str1, string str2) : str1_(std::move(str1)), str2_(std::move(str2)) {}
+	static constexpr int kMaxLengthScore = 60;
 
+	size_t GetScore()
+	{
+		return GetLengthScore();
+	}
+
+	void SetStrings(string str1, string str2)
+	{
+		str1_ = std::move(str1);
+		str2_ = std::move(str2);
+	}
+
+private:
+	size_t GetLengthScore()
+	{
+		const auto long_str_len = max(str1_.size(), str2_.size());
+		const auto short_str_len = min(str1_.size(), str2_.size());
+
+		if (!long_str_len)
+		{
+			return 0;
+		}
+		auto gap = long_str_len - short_str_len;
+
+		if (gap >= short_str_len)
+		{
+			return 0;
+		}
+
+		if (!gap)
+		{
+			return kMaxLengthScore;
+		}
+		return kMaxLengthScore - gap * kMaxLengthScore / short_str_len;
+	}
+
+	string str1_;
+	string str2_;
 };
--- SC-Test/test.cpp
+++ SC-Test/test.cpp
@@ -1,6 +1,48 @@
 #include "pch.h"
 #include "../ProjectSC/SimilarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
+class SimilarityCheckerTest : public ::testing::Test
+{
+public:
+	void SetUp() override
+	{
+		similarity_checker_ = std::make_unique<SimilarityChecker>();
+	}
 
+	void SetStrings(string str1, string str2) const
+	{
+		similarity_checker_->SetStrings(std::move(str1), std::move(str2));
+	}
+
+	size_t GetScore()
+	{
+		return similarity_checker_->GetScore();
+	}
+
+private:
+	std::unique_ptr<SimilarityChecker> similarity_checker_;
+};
+
+TEST_F(SimilarityCheckerTest, LengthTest1) {
+	int expected = 60;
+	SetStrings("ASD", "DSA");
+	EXPECT_EQ(GetScore(), expected);
+}
+
+TEST_F(SimilarityCheckerTest, LengthTest2) {
+	int expected = 0;
+	SetStrings("A", "BB");
+	EXPECT_EQ(GetScore(), expected);
+}
+
+TEST_F(SimilarityCheckerTest, LengthTest3) {
+	int expected = 20;
+	SetStrings("AAABB", "BAA");
+	EXPECT_EQ(GetScore(), expected);
+}
+
+TEST_F(SimilarityCheckerTest, LengthTest4) {
+	int expected = 30;
+	SetStrings("AA", "AAA");
+	EXPECT_EQ(GetScore(), expected);
 }
